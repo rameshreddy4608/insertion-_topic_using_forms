6cbf7f4cb275f13b4ccad5f9a6c0c44504443fac
rameshreddy <rameshreddy4608@gmail.com>
2023-04-14 14:52:11 +0530

    first commit

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from django.db import models
+
+# Create your models here.
+
+class Topic(models.Model):
+    topic_name=models.CharField(max_length=100,primary_key=True)
+
+    def __str__(self) -> str:
+        return self.topic_name
+    
+class Webpage(models.Model):
+    topic_name=models.ForeignKey(Topic,on_delete=models.CASCADE)
+    name=models.CharField(max_length=100)
+    url=models.URLField()
+
+    def __str__(self) -> str:
+        return self.name
+    
+class Accessrecords(models.Model):
+    name=models.ForeignKey(Webpage,on_delete=models.CASCADE)
+    authour=models.CharField(max_length=100)
+    date=models.DateField()
+
+    def __str__(self):
+        return self.authour
